7d2f8d663bbb3129d52e47faedd4fe68793228d9
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-11-27 17:07:01 +0500

    Added rotation view component

---
+++ Assets/Scripts/RotatableView.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace RenderingExperiments
+{
+    public class RotatableView : MonoBehaviour
+    {
+        [SerializeField] private bool _randomizeAxisOnStart;
+        [SerializeField] private Vector3 _axis;
+        [SerializeField] private float _speed = 100;
+        
+        private const int MinRandomAngle = -100;
+        private const int MaxRandomAngle = 100;
+
+        private void Start()
+        {
+            if (_randomizeAxisOnStart)
+            {
+                RandomizeAxis();
+            }
+        }
+
+        private void Update()
+        {
+            transform.Rotate(_axis.normalized * Time.deltaTime * _speed);
+        }
+
+        private void RandomizeAxis()
+        {
+            _axis = new Vector3
+            (
+                Random.Range(MinRandomAngle, MaxRandomAngle),
+                Random.Range(MinRandomAngle, MaxRandomAngle),
+                Random.Range(MinRandomAngle, MaxRandomAngle)
+            ).normalized;
+        }
+    }
+}
